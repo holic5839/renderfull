cf43c0093c8270d9c65682cfdb635757bd82192c
slkawoon <slkawoon.koo@purpleworks.co.kr>
2019-07-05 17:23:11 +0900

    설정 변수 추가

--- pages/imgBox.js
+++ pages/imgBox.js
@@ -2,30 +2,35 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 const ImgBox = (data) => {
-  const maxWidth = data.col * 465;
-  const minWidth = data.col * 465;
+  const maxWidth = data.setting.col * 465;
+  const minWidth = data.setting.col * 465;
   
   const Container = styled.div`
     margin: 0 auto;
     max-width: ${maxWidth}px;
     min-width: ${minWidth}px;
   `
-  
-  const Image = styled.img`
+  const ImageWrapper = styled.div`
+    display: inline-block;
     width: 450px;
-    height: 480px;
+    height: 620px;
     margin: 0 0 37px 15px;
-
-    ${props => props.banner && css`
-      width: 1080px;
-    `}
   `
-  console.log(data.url[1]);
+
+  const Image = styled.img`
+    width: 100%;
+  `
+console.log('설정 \n', data.setting);
   return (
     <Container>
       {
-        data.url.map((image) => (
-        <Image src={`${image}`}/>
+        data.setting.url.map((image) => (
+          <ImageWrapper key={`image-wrapper__${image}`}>
+            <Image src={`${image}`} />
+            <h2>test</h2>
+            <h3>test</h3>
+            <span>test</span>
+          </ImageWrapper>
       ))}
     </Container>
   );
